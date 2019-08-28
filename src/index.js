8ba3644f9cb2c823a1483dd668a04c9151ee6f8c
Mukesh <mukeshpc97@gmail.com>
2019-08-27 20:35:19 -0400

    Update
    IMPLEMENT Scroll component
    IMPLEMENT loading functionality
    Get data from jsonplaceholder website

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 import 'tachyons';
 
-import App from './App';
+import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
