6ba6d6433075b7b47c1be8e924d4fa9160321db6
Mukesh <mukeshpc97@gmail.com>
2019-08-30 16:57:15 -0400

    Build

---
+++ build/precache-manifest.2ec67eb6c7ece58b3c1d14681c1538b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ae97c8d9c5308c5d54acd24d7387f9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b0200a5ac53e8864e75c",
+    "url": "/robofriends/static/css/main.5b5adf3e.chunk.css"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/robofriends/static/js/2.52355532.chunk.js"
+  },
+  {
+    "revision": "b0200a5ac53e8864e75c",
+    "url": "/robofriends/static/js/main.f7151973.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
